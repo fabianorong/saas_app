1148f0487ca13e33b7db3ce7009cf9d82145c9bb
Fabiano Rong <fabianorong@gmail.com>
2024-09-30 14:41:45 -0300

    final adjustments

--- vite-project/src/sections/Pricing.jsx
+++ vite-project/src/sections/Pricing.jsx
@@ -10,7 +10,7 @@ const Pricing = () => {
 
   return (
     <section>
-      <Element name="pricing">
+      <Element name="Preços">
         <div className="container">
           <div className="max-w-960 pricing-head_before relative mx-auto botder-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16 ">
             <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
